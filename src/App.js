a2073a3e238dd0bb8868f28ba2746d057eaf4cc9
Thiago Coutinho <thiago.coutinho@nuuvem.com>
2021-11-20 19:34:59 -0300

    Add app final structure

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-// Components
 import MovieReviewsList from './components/moviesReviewsList';
+import LoadMore from './components/loadMore';
+import Modal from './components/modal';
 import { FaVideo } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="antialiased bg-gray-100 min-h-screen"> 
-      {/* NAVBAR */}
-      <div className="bg-white h-16 border-b-2 border-gray-200"> 
-        <div className="container mx-auto"> 
-           <div className="flex items-center h-16">
+    <div className="antialiased bg-blue-100 xl:h-screen xl:max-h-screen xl:overflow-hidden">
+      <div className="bg-white h-16 border-b-2 border-gray-200">
+        <div className="sm:container sm:mx-auto">
+           <div className="flex items-center h-16 px-4">
              <h1 className="flex items-center gap-2">
               <FaVideo/>
-              <span className="font-semibold uppercase">Coolmovies</span>
+              <span className="font-title font-semibold uppercase">Coolmovies</span>
              </h1>
            </div>
         </div>
       </div>
-      <div className="container mx-auto py-8">
-        <p className="text-lg">
-          <span>Welcome back, <span className="font-bold">Chrono</span>!</span>
-        </p>
-
-        <p className="text-2xl font-semibold mt-4">
-          Check the latest reviews:
-        </p>
-
-        {/* Filters */}
-        {/* <div className="flex items-center mt-2">
-          <span className="px-2 py-1 bg-green-100 border border-green-600 text-green-600 rounded-full text-sm cursor-pointer">
-            Show mine first
-          </span>
-        </div> */}
-
-        <div className="flex gap-4 my-4">
-          <MovieReviewsList />
-        </div>     
-      </div>  
+      <div className="sm:container sm:mx-auto">
+        <div className="p-4 pt-8 pb-0 sm:py-8 sm:pb-0">
+          <p className="text-lg font-title">
+            <span>Welcome back, <span className="font-bold">Admin</span>!</span>
+          </p>
+          <p className="font-title text-xl sm:text-2xl font-bold mt-4">
+            Your team latest reviews
+          </p>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 my-4 font-description">
+            <MovieReviewsList />
+          </div>
+          <LoadMore />
+        </div>
+      </div>
+      <Modal/>
     </div>
   );
 }
