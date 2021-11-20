d0ca5ac98e29cf6f575d9fb78630dda24d115c83
Thiago Coutinho <thiago.coutinho@nuuvem.com>
2021-11-20 19:31:11 -0300

    Setup dependencies config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 // tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': '340px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      animation: {
+        'fade-in' : 'fade-in .4s linear forwards'
+      },
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        }
+       }
+    },
+    fontFamily: {
+      'title': ['Raleway'],
+      'description': ['Open Sans'],
+    }
   },
   variants: {
     extend: {},
